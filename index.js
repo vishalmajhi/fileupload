16881e7e3d4c525bf95019014ceabd9f53a2551e
vishal majhi <vishalmajhi33@gmail.com>
2023-11-19 20:28:39 +0530

    multifield

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+// git remote add origin https://github.com/vishalmajhi/fileupload.git
+// >>   git branch -M main
+// >>   git push -u origin main
 
 const express = require("express");
 const app =express();
@@ -15,9 +17,23 @@ const upload =multer({dest:"uplods"});
 
 //multiple file
                                     //  for number file upload
-app.post("/upload",upload.array("file" ,2),(req,res)=>{
+
+
+// app.post("/upload",upload.array("file" ,2),(req,res)=>{
+//     res.json({status:"sucess"})
+// })
+
+// multiple field upload
+const multifield =upload.fields([
+    {name:"avatar",maxCount: 1  
+},
+{name:"profile",maxCount: 1  
+}
+]);
+app.post("/upload",multifield,(req,res)=>{
     res.json({status:"sucess"})
 })
+
 
 app.listen(5000,()=>{
     console.log("server is running");
